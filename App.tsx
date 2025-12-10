d2d77e3a8d304ceb616ca63aa203693038174d0b
JanneM11 <jannem92@outlook.com>
2025-12-10 19:17:58 +0200

    tehty

--- App.tsx
+++ App.tsx
@@ -1,11 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { CameraView, useCameraPermissions, BarcodeType } from 'expo-camera';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [permission, requestPermission] = useCameraPermissions();
+  const [scanning, setScanning] = useState(false)
+  const [scannedData, setScannedData] = useState<string | null>(null)
+  const BARCODETYPES: BarcodeType[] = [
+    "aztec",
+    "ean13",
+    "ean8",
+    "qr",
+    "pdf417",
+    "upc_e",
+    "datamatrix",
+    "code39",
+    "code93",
+    "itf14",
+    "codabar",
+    "code128",
+    "upc_a",
+  ];
+
+  if (!permission) {
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+    function handleBarcodeScanned({ type, data }: { type: string; data: string }) {
+    setScanning(false); 
+    setScannedData(data);
+  }
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <CameraView           
+        style={styles.camera}
+        facing="back"
+        onBarcodeScanned={scanning ? handleBarcodeScanned : undefined}
+        barcodeScannerSettings={{
+        barcodeTypes: BARCODETYPES,
+        }} 
+      />
+      <View style={styles.buttonContainer}>
+      {scannedData && (
+        <Text style={styles.message}>Barcode: {scannedData}</Text>
+      )}
+      
+      <Button 
+        title="Scan barcode"
+        onPress={() => { 
+          setScannedData(null)
+          setScanning(true)
+        }}
+      />
+      </View>
     </View>
   );
 }
@@ -13,8 +72,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+  },
+  message: {
+    color:"#fff",
+    textAlign: 'center',
+    paddingBottom: 10,
+    backgroundColor: "#000",
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 80,
+    width: '70%',
+    alignSelf: 'center'
   },
 });
